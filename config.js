d9d89c55622efbce00a9dba9eea668499b89b2b1
RabbitMxs <42190672+RabbitMxs@users.noreply.github.com>
2020-05-11 13:52:56 -0500

    change config2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-    port: process.env.port||3000,
-    db: process.env.MONGODB_URI||'mongodb://localhost:27017/shop',
+    port: process.env.port || 3000,
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
     SECRET_TOKEN: 'miclavedetokens'
 }
